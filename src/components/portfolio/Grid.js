09e7a3ab3a2c8e4bc02eb609fa8ec6bf478552a3
Asam Shan <asamshan456@gmail.com>
2020-02-18 14:32:35 +0000

    updating projects

--- src/components/portfolio/Grid.js
+++ src/components/portfolio/Grid.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { StyledA } from "../shared/Shared.js"
 const data = require("./data.json")
+
 export default function Grid() {
   return (
     <GridContainer>
